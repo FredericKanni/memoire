a4673603afb510f62b2d58d767ae325d2058320d
FREDERIC <kannif@ymail.com>
2019-11-18 22:46:59 +0400

    ajout formulaire pour la creation du media

--- resources/views/crud.blade.php
+++ resources/views/crud.blade.php
@@ -5,5 +5,19 @@
 
 {{-- le contenu sera placer ds la section content et sera appeler sur le layout  la ou on a mis un yield --}}
 @section('content')
-   crud
+  <h1>CRUD</h1>
+  <hr>
+  <h3>media</h3>
+  <p>creer un media</p>
+  <form>
+    <div class="form">
+    
+        <input type="text" class="form-control" placeholder="nom du media">
+   
+      
+        <button type="submit" class="btn btn-primary w-100">creer</button>
+      
+    </div>
+  </form>
+  <hr>
 @endsection 
